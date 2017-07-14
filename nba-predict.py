9fa86c4b6b6508d768496303f46b29e9a8d4f14f
Sidharth Rajaram <sidharthrajaram@gmail.com>
2017-07-13 17:42:36 -0700

    added wholesome prediction source file

---
+++ nba-predict.py
@@ -0,0 +1,9 @@
+import numpy as np
+from termcolor import colored
+from sklearn import svm
+
+import matplotlib.pyplot as plt #visualization
+from matplotlib import style # ^
+style.use("ggplot")
+
+# lol
