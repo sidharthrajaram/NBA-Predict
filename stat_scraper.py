d5102dd54e6154cabcc20c4d596e41ad9693a55a
Shiv Ansal <ansalshiv@gmail.com>
2017-07-14 18:30:19 -0700

    cse id and google search api python

--- stat_scraper.py
+++ stat_scraper.py
@@ -1,6 +1,20 @@
-import pandas as pd
-all_players = ["a"]
+from googleapiclient.discovery import build
+import pprint
 
+my_api_key = "AIzaSyC-djDYtVMjEJSubc8oaE-W-EEUrHJD0g0"
+my_cse_id = "013234493367067861201:e_sqh9dvrhy"
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
+
+results = google_search(
+    'westbrook', my_api_key, my_cse_id, num=1)
+for result in results:
+    print(result.keys())
+
+'''
 def get_unique_id(player):
 	return "1234"
 
@@ -22,3 +36,4 @@ for player in all_players:
 		#Figure out first season
 		#Access page 
 		#SCRAPE
+'''
