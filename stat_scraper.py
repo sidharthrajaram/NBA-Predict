67a6e824f2d81d112b0239e3be3c7ee9fb4257a4
Shiv Ansal <ansalshiv@gmail.com>
2017-07-14 19:45:36 -0700

    finished scraper

--- stat_scraper.py
+++ stat_scraper.py
@@ -1,5 +1,8 @@
 from googleapiclient.discovery import build
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
 
 my_api_key = "AIzaSyC-djDYtVMjEJSubc8oaE-W-EEUrHJD0g0"
 my_cse_id = "013234493367067861201:e_sqh9dvrhy"
@@ -9,24 +12,15 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
     return res['items']
 
-results = google_search('westbrook rookie gamelog', my_api_key, my_cse_id, num=1)
+results = google_search(
+    'westbrook  ', my_api_key, my_cse_id, num=1)
 print results[0]["formattedUrl"]
-a = pd.read_html("https://"+results[0]["formattedUrl"])
+stats = pd.read_html("https://"+results[0]["formattedUrl"])
+df = pd.DataFrame(stats[0])
 
-'''
+for index, row in df.iterrows():
+	if index == 0:
+		rookie_stats = row
 
-for player in all_players:
-	player_id = get_unique_id(player)
-	first_season = get_player_start(player_id)
-	url = "http://www.basketball-reference.com/players/g/"+player_id"+/gamelog/"+first_season
-
-	a = pd.read_html("http://www.basketball-reference.com/players/g/georgpa01/gamelog/2011")
-	#im too tired to write more of this shit 
-
-
-	#For Each player 
-		#Get unique website ID
-		#Figure out first season
-		#Access page 
-		#SCRAPE
-'''
+print rookie_stats
+print (np.asarray(rookie_stats))[5:]
