4b30c561b4081d4ba5547b687b2df545c222af1d
Sidharth Rajaram <sidharthrajaram@gmail.com>
2021-03-07 11:50:36 -0800

    cleaned up comments

--- multi.py
+++ multi.py
@@ -25,14 +25,10 @@ classifier = svm.SVC()
 #offense
 # #TS% vs PPG
 scoring_ability = combineData(X[:,3], X[:,45])
-# # #3Pr vs 3P%
-# threept = combineData(X[:,4], X[:,29])
 # #FTr vs FT%
 ft = combineData(X[:,5], X[:,36])
 # #ast vs TOV
 passing = combineData(X[:,40], X[:,43])
-# # #OWS vs WS
-# off_presence = combineData(X[:,14], X[:,16])
 
 def offense(player):
 	result = 0.0
@@ -41,28 +37,11 @@ def offense(player):
 	classifier.fit(scoring_ability, Y)
 	result += classifier.predict([[ player[1][3], player[0][23] ]])[0]
 
-	# classifier.fit(ft, Y)
-	# result += classifier.predict([[ player[1][4], player[0][14] ]])[0]
-
-	# # #FTr vs FT%
-	# classifier.fit(ft, Y)
-	# result += classifier.predict([[ player[1][5], player[0][14] ]])[0]
-
-	# # #AST vs TOV
-	# classifier.fit(passing, Y)
-	# result += classifier.predict([[ player[0][18], player[0][21] ]])[0]
-
-	# # #OWS vs WS
-	# classifier.fit(off_presence, Y)
-	# result += classifier.predict([[ player[1][15], player[1][17] ]])[0]
-
 	print("STAR Offensive Rating:",result)
 	return result
 
 
 #defense
-# #DWS vs WS
-# def_presence = combineData(X[:,15], X[:,16])
 # #STL vs BLK
 stalwart = combineData(X[:,41], X[:,42])
 # #DBPM vs DBPM
@@ -81,10 +60,6 @@ def defense(player):
 	classifier.fit(def_rating, Y)
 	result += classifier.predict([[ player[1][21], player[1][21] ]])[0]
 
-	# #pf vs pf
-	# classifier.fit(fouls, Y)
-	# result += classifier.predict([[ player[0][22], player[0][22] ]])[0]
-
 	print("STAR Defensive Rating:",result)
 	return result
 
@@ -94,10 +69,6 @@ def defense(player):
 efficiency_set = combineData(X[:,13], X[:,2])
 # #USG/TOV%
 further_set = combineData(X[:,13], X[:,12])
-# #MPG vs TOV
-# turnovers = combineData(X[:,23], X[:,43])
-# # #FGA vs FGM
-# scoring_eff = combineData(X[:,24], X[:,25])
 # #PER/PER lol
 per = combineData(X[:,2], X[:,2])
 
@@ -112,14 +83,6 @@ def efficiency(player):
 	classifier.fit(further_set, Y)
 	result += classifier.predict([[ player[1][13], player[1][12] ]])[0]
 
-	# # #MPG vs TOV
-	# classifier.fit(turnovers, Y)
-	# result += classifier.predict([[ player[0][1], player[0][21] ]])[0]
-
-	# # #FGA vs FGM
-	# classifier.fit(scoring_eff, Y)
-	# result += classifier.predict([[ player[0][3], player[0][2] ]])[0]
-
 	# #PER/PER lol
 	classifier.fit(per, Y)
 	result += classifier.predict([[ player[1][2], player[1][2] ]])[0]
@@ -129,12 +92,8 @@ def efficiency(player):
 
 
 #durability
-# # #GP/WS48
-# durability_set = combineData(X[:,0], X[:,17])
 # #GP/MP
 play = combineData(X[:,0], X[:,1])
-# # #MP/WS48
-# perforty = combineData(X[:,1], X[:,17])
 
 def durability(player):
 	result = 0.0
@@ -150,10 +109,6 @@ def durability(player):
 #value
 # #VORP/WS
 value_set = combineData(X[:,21], X[:,21])
-# VORP/WS48
-# game_forty = combineData(X[:,21], X[:,17])
-#BPM / WS
-# box = combineData(X[:,20], X[:,16])
 
 def value(player):
 	result = 0.0
@@ -161,10 +116,7 @@ def value(player):
 	# #VORP/VORp
 	classifier.fit(value_set, Y)
 	result += classifier.predict([[ player[1][23], player[1][23] ]])[0]
-
-	# #BPM / WS
-	# classifier.fit(box, Y)
-	# result += classifier.predict([[ player[1][22], player[1][17] ]])[0]
+	
 	print("STAR Value Rating:",result)
 	return result
 
@@ -177,8 +129,8 @@ def compositePredict(player):
 	result += 1.5*(durability(player))
 	# result += value(player)
 	result = 1.25*result
-# 	if(result >= 5):
-# 		result = 5
+ 	if(result >= 10):
+ 		result = 10
 	return round(result,3)
 
 def offenseSkillWord(player):
